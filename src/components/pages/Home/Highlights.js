fedf5a457a07d3eb61c843fb86be52ac5160e3a5
Jikku Alex <iamjikkualex@gmail.com>
2023-08-06 00:25:43 +0530

    Added Home page changes

--- src/components/pages/Home/Highlights.js
+++ src/components/pages/Home/Highlights.js
@@ -1,8 +1,60 @@
+import { Link } from 'react-router-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faMotorcycle } from '@fortawesome/free-solid-svg-icons';
+import greekSaladImage from './assets/images/greek-salad.jpg';
+import bruschettaImage from './assets/images/bruschetta.jpg';
+import lemonDessertImage from './assets/images/lemon-dessert.jpg';
+import AppUtils from '../../../utils/AppUtils';
+import './assets/styles/Highlights.css';
+
+const specials = [
+    {
+        name: 'Greek Salad',
+        image: greekSaladImage,
+        price: '$12.99',
+        description: 'The famous greek salad of crispy lettuce, peppers, olives and our Chicago style feta cheese, garnished with crunchy garlic and rosemary croutons.',
+    },
+    {
+        name: 'Bruschetta',
+        image: bruschettaImage,
+        price: '$7.99',
+        description: 'Our Bruschetta is made from grilled bread that has been smeared with garlic and seasoned with salt and olive oil.',
+    },
+    {
+        name: 'Lemon Dessert',
+        image: lemonDessertImage,
+        price: '$5.99',
+        description: 'A simple, zingy, creamy no-bake tasty lemon dessert with impressive pud, light enough to have a slice to finish a big meal.'
+    },
+];
+
 const Highlights = () => {
     return (
-        <>
-        
-        </>
+        <section className="container grid highlights">
+            <div className="highlights-header">
+                <h2>Specials</h2>
+                <Link className='button-primary' to={AppUtils.links.get('orderOnline').path}>
+                    Online Menu
+                </Link>
+            </div>
+            {specials.map((special, index) =>
+                <article key={index} className='special-card'>
+                    <div className='special-card-image'>
+                        <img src={special.image} alt={special.name} />
+                    </div>
+                    <div className='special-card-header'>
+                        <h3>{special.name}</h3>
+                        <span>{special.price}</span>
+                    </div>
+                    <div className='special-card-body'>
+                        <p>{special.description}</p>
+                        <Link to={AppUtils.links.get('orderOnline').path}>
+                            Order a Delivery <FontAwesomeIcon icon={faMotorcycle} />
+                        </Link>
+                    </div>
+                </article>
+            )}
+        </section>
     );
 };
 
--- src/components/pages/Home/Testimonials.js
+++ src/components/pages/Home/Testimonials.js
@@ -1,8 +1,59 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faStar, faStarHalfStroke } from '@fortawesome/free-solid-svg-icons';
+import customer01Photo from './assets/images/customer1.jpg';
+import customer02Photo from './assets/images/customer2.jpg';
+import customer03Photo from './assets/images/customer3.jpg';
+import customer04Photo from './assets/images/customer4.jpg';
+import './assets/styles/Testimonials.css';
+
+const reviews = [
+    {
+        customerName: 'Ann Ben',
+        customerPhoto: customer01Photo,
+        rating: [1, 1, 1, 1],
+        review: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+    },
+    {
+        customerName: 'Matthew Henry',
+        customerPhoto: customer02Photo,
+        rating: [1, 1, 1],
+        review: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+    },
+    {
+        customerName: 'Jeff Haris',
+        customerPhoto: customer03Photo,
+        rating: [1, 1, 1, 0.5],
+        review: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+    },
+    {
+        customerName: 'Kris Tom',
+        customerPhoto: customer04Photo,
+        rating: [1, 1, 1, 1, 0.5],
+        review: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+    },
+];
+
+const ratingLevels = {'0.5': faStarHalfStroke, '1': faStar, };
+
 const Testimonials = () => {
     return (
-        <>
-        
-        </>
+        <section className="testimonials">
+            <div className="container grid">
+                <h2>Testimonials</h2>
+                {reviews.map((review, index01) =>
+                    <article key={index01} className='testimonial-card'>
+                        <img src={review.customerPhoto} alt={review.customerName} />
+                        <h4>{review.customerName}</h4>
+                        <span>
+                            {review.rating.map((rate, index02) =>
+                                <FontAwesomeIcon key={index02} icon={ratingLevels[rate]} size='xs' />
+                            )}
+                        </span>
+                        <blockquote><p>"{review.review}"</p></blockquote>
+                    </article>
+                )}
+            </div>
+        </section>
     );
 };
 
